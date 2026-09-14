0fbd82d02ed68cbcd9b14441d4e19cdeda88000b
Dhiraj Kumar Gupta (Dhiru) <dhirajkumarg413@gmail.com>
2026-09-14 13:02:10 +0530

    Time: 379 ms (5.15%), Space: 19.4 MB (5.59%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,51 +1,37 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& img1,
-                       vector<vector<int>>& img2) {
-
-        int n = img1.size();
-        int ans = 0;
-
-        // Try every possible vertical shift
-        for (int dr = -(n - 1); dr <= n - 1; dr++) {
-
-            // Try every possible horizontal shift
-            for (int dc = -(n - 1); dc <= n - 1; dc++) {
-
-                int overlap = 0;
-
-                // Check every cell of img1
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-
-                        // New position of img1[i][j]
-                        // after applying the translation
-                        int ni = i + dr;
-                        int nj = j + dc;
-
-                        // Check whether translated position
-                        // is inside the matrix
-                        if (ni >= 0 && ni < n &&
-                            nj >= 0 && nj < n) {
-
-                            // If both images have 1,
-                            // then we get an overlap
-                            if (img1[i][j] == 1 &&
-                                img2[ni][nj] == 1) {
-                                overlap++;
-                            }
-                        }
-                    }
-                }
-
-                // Store maximum overlap
-                ans = max(ans, overlap);
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+       vector<pair<int,int>>a,b;
+       int ans=0;
+       int n=img1.size();
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(img1[i][j]==1){
+                a.push_back({i,j});
             }
         }
+       }
 
-        return ans;
+        for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(img2[i][j]==1){
+                b.push_back({i,j});
+            }
+        }
+       }
+       unordered_map<string,int>mpp;
+       for(auto [x1,y1]:a){
+        for(auto [x2,y2]:b){
+            //relative position;
+            int dx=x2-x1;
+            int dy=y2-y1;
+            string s=to_string(dx)+","+to_string(dy);
+            mpp[s]++;
+            ans=max(ans,mpp[s]);
+
+        }
+       }
+       return ans;
+     
     }
 };
