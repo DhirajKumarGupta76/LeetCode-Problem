836389cfe723490bca6519f134e87b3d7515dd6b
Dhiraj Kumar Gupta (Dhiru) <dhirajkumarg413@gmail.com>
2026-09-19 00:10:53 +0530

    Time: 19 ms (42.4%), Space: 23.3 MB (88.07%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -4,46 +4,48 @@ public:
 
         int n = s.size();
 
-        vector<int> start(26, n);
-        vector<int> end(26, -1);
+        vector<int> start(26, -1);
+        vector<int> end(26, 0);
         vector<bool> isValid(26, true);
 
-        // Find first and last occurrence of every character
+        // Find first and last occurrence
         for (int i = 0; i < n; i++) {
 
             int c = s[i] - 'a';
 
-            start[c] = min(start[c], i);
+            // First occurrence
+            if (start[c] == -1) {
+                start[c] = i;
+            }
+
+            // Last occurrence
             end[c] = i;
         }
 
-        // Check every character
+        // Check whether each character can form a valid substring
         for (int c = 0; c < 26; c++) {
 
-            // Character does not exist
-            if (end[c] == -1)
+            // Character is not present
+            if (start[c] == -1)
                 continue;
 
-            // Check the complete range of this character
             for (int i = start[c]; i <= end[c]; i++) {
 
                 int x = s[i] - 'a';
 
-                // This character occurs before start[c]
+                // Character occurs before start[c]
                 if (start[x] < start[c]) {
                     isValid[c] = false;
                     break;
                 }
 
-                // Extend the ending position
+                // Expand the ending position
                 end[c] = max(end[c], end[x]);
             }
         }
 
         vector<string> result;
 
-        // Keep track of the starting position
-        // of the last selected substring
         int lastTakenStart = INT_MAX;
 
         // Traverse from right to left
@@ -51,12 +53,11 @@ public:
 
             int c = s[i] - 'a';
 
-            // Invalid substring
             if (!isValid[c])
                 continue;
 
-            // i must be the starting position of this substring
-            // and it should not overlap with previous selection
+            // i is the starting position
+            // and substring does not overlap
             if (i == start[c] && end[c] < lastTakenStart) {
 
                 result.push_back(
